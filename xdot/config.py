278188c0473eb6ff229ef567c918c44c9a28e2cd
megatron-thx <megatron.thx@vx1>
2026-05-20 21:04:19 +0400

    html frame y_correction_factor implemented

--- xdot/config.py
+++ xdot/config.py
@@ -9,11 +9,13 @@ class Config:
     multi_line_activate: bool
     line_separator: str
     dark_theme: bool
+    y_correction_factor: float
 
 config = Config(
     multi_line_activate = False,
     line_separator = ";",
-    dark_theme = False
+    dark_theme = False,
+    y_correction_factor = 7.232
 )
 
 print(config)
